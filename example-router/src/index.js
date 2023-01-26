5ec22cfe1a513b67b809f7f279c5af543db7d6d9
Lassi Kuisma <lassi.kuisma1@gmail.com>
2023-01-26 20:50:23 +0200

    example: styled components

--- example-router/src/index.js
+++ example-router/src/index.js
@@ -5,20 +5,43 @@ import {
   Routes, Route, Link, useNavigate, Navigate, useMatch
 } from "react-router-dom"
 import {
-  Alert,
-  Container,
   Table,
   TableBody,
   TableCell,
   TableContainer,
   TableRow,
   Paper,
-  TextField,
-  Button,
-  AppBar,
-  Toolbar,
-  IconButton,
 } from '@mui/material'
+import styled from 'styled-components'
+
+const Button = styled.button`
+  background: Bisque;
+  font-size: 1em;
+  margin: 1em;
+  padding: 0.25em 1em;
+  border: 2px solid Chocolate;
+  border-radius: 3px;
+`
+
+const Input = styled.input`
+  margin: 0.25em;
+`
+
+const Page = styled.div`
+  padding: 1em;
+  background: papayawhip;
+`
+
+const Navigation = styled.div`
+  background: BurlyWood;
+  padding: 1em;
+`
+
+const Footer = styled.div`
+  background: Chocolate;
+  padding: 1em;
+  margin-top: 1em;
+`
 
 const Home = () => (
   <div> <h2>Notes app</h2> </div>
@@ -74,13 +97,15 @@ const Login = (props) => {
       <h2>Login</h2>
       <form onSubmit={onSubmit}>
         <div>
-          <TextField label="Username" />
+          Username:
+          <Input />
         </div>
         <div>
-          <TextField label="Password" type="password" />
+          Password:
+          <Input type="password" />
         </div>
         <div>
-          <Button variant="contained" color="primary" type="submit">
+          <Button type="submit" primary=''>
             Login
           </Button>
         </div>
@@ -124,33 +149,16 @@ const App = () => {
     : null
 
   return (
-    <Container>
-      <div>
-        {(message &&
-          <Alert severity="success">
-            {message}
-          </Alert>
-        )}
-      </div>
-
-      <AppBar position="static">
-        <Toolbar>
-          <Button color="inherit" component={Link} to="/">
-            Home
-          </Button>
-          <Button color="inherit" component={Link} to="/notes">
-            Notes
-          </Button>
-          <Button color="inherit" component={Link} to="/users">
-            Users
-          </Button>
-          {user
-            ? <em>{user} logged in</em>
-            : <Button color="inherit" to="/login">Login</Button>
-          }
-        </Toolbar>
-      </AppBar>
-
+    <Page>
+      <Navigation>
+        <Link style={padding} to="/">home</Link>
+        <Link style={padding} to="/notes">notes</Link>
+        <Link style={padding} to="/users">users</Link>
+        {user
+          ? <em>{user} logged in</em>
+          : <Link style={padding} to="/login">login</Link>
+        }
+      </Navigation>
 
       <Routes>
         <Route path="/notes/:id" element={<Note note={note} />} />
@@ -160,10 +168,10 @@ const App = () => {
         <Route path="/" element={<Home />} />
       </Routes>
 
-      <div>
-        <i>Note app, made by me :)</i>
-      </div>
-    </Container>
+      <Footer>
+        <em>Note app, made by me :)</em>
+      </Footer>
+    </Page>
   )
 }
 
